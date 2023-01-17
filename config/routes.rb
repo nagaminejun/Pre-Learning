f498b3e007a6aa4db3dde864881727b50e51a7a2
nagaminejun <nagaminejun1988@gmail.com>
2023-01-17 12:50:37 +0900

    gem devise を導入、userの新規作成とログイン確認OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :user1s
+  devise_for :users
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  get 'comments/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_11_032543) do
-
-  create_table "user1s", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_user1s_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_user1s_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 2023_01_12_032547) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_01_11_032543) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
