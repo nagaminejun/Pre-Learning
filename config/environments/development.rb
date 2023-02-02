825f2a4ab49c0c1d01853945f45b6707ee1cbe9f
nagaminejun <nagaminejun1988@gmail.com>
2023-02-02 11:36:50 +0900

    devise_1の確認作業終了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -34,9 +37,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -74,7 +77,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web #, :smtp
   config.action_mailer.perform_deliveries = true 
+  
+
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'nagaminejun1988@gmail.com', #'送信元となるGmailアドレスを記入', #notifications@example.com
+    password:             'yotideyisolqlqej',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"#"devise/registrations" #ここに"users/registrations"を入れるとルーティングエラーなる、、
+  }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  get 'comments/index'
+  root 'comments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
+
 
